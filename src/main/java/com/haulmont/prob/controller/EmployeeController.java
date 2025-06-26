bc2e8095a57fca1c4630d43f6b4e817072e6bb40
aniam <aniamurzina@gmail.com>
2025-06-26 20:20:29 +0400

    getting statistics by tezis_id

--- src/main/java/com/haulmont/prob/controller/EmployeeController.java
+++ src/main/java/com/haulmont/prob/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.haulmont.prob.model.Employee;
 import com.haulmont.prob.model.Statistics;
 import com.haulmont.prob.repository.EmployeeRepository;
 import com.haulmont.prob.repository.StatisticsRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -14,6 +15,7 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
@@ -62,19 +64,26 @@ public class EmployeeController {
 
     /**
      * Получение статистики по конкретному сотруднику (по tezisId)
+     * получение всей статистики по конкретному пользователю по tezis id employee потом по нему статистику всю и вернуть в формате джсон
      */
     @GetMapping("/{tezisId}/statistics")
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<?> getEmployeeStatistics(@PathVariable UUID tezisId) {
+        log.info("REQUESTING!!!!!! statistics for tezisId: {}", tezisId);
         try {
             Optional<Employee> employee = employeeRepository.findByTezisId(tezisId);
             if (employee.isEmpty()) {
+                log.warn("Employee not found for tezisId: {}", tezisId);
                 return ResponseEntity.notFound().build();
             }
+            log.info("Found employee: {}", employee.get().getId());
 
             List<Statistics> statistics = statisticsRepository.findByEmployeeId(employee.get().getId());
+            log.info("Found {} statistics records", statistics.size());
+
             return ResponseEntity.ok(statistics);
         } catch (Exception e) {
+            log.error("Error retrieving statistics", e);
             return ResponseEntity.internalServerError().body("Error retrieving statistics");
         }
     }
